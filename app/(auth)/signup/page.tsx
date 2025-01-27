aa13e12f2f711db93e4f3b8b26ca9fa322ea051c
Abhishek <rambopranjal63@gmail.com>
2025-01-27 22:32:25 +0530

    responsive of login

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import Signup from "@/components/Signup";
 
 export default function signup(){
      return(
-            <div className="h-[calc(100vh-3.5rem)] w-full flex justify-center items-center mx-auto">
+            <div className="h-full w-full flex justify-center items-center mx-auto">
                 <Signup/>
             </div>
         )

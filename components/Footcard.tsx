31082d49e8f1274e4b90bcec6fe74621bd09de03
Abhishek <rambopranjal63@gmail.com>
2025-01-27 23:08:19 +0530

    Update Footcard.tsx

--- components/Footcard.tsx
+++ components/Footcard.tsx
@@ -6,7 +6,7 @@ interface Stock {
 
 export default function Footcard({name,price}:Stock){
     return(
-        <div className="bg-green-600 rounded-3xl w-56 h-14 flex justify-evenly items-center text-white text-xl">
+        <div className="bg-green-600 rounded-3xl w-56 h-14 sm:flex justify-evenly items-center text-white sm:text-xl text-base text-center">
             <div className="font-bold ">{name}</div>
             <div className="font-semibold animate-pulse">{price}</div>
         </div>
